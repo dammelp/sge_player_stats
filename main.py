49082ab9c4ace6a7c688ddcc208cf416dee1bbde
dammelp <dammelp@gmail.com>
2024-10-04 18:20:19 +0200

    added post request, made columns optional

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import jsonify, request
 from app import app
 from player_model import Player, db
@@ -9,6 +11,77 @@ def retrieve_all_player():
     all_players = result.scalars().all()
     all_players_serialized = [player.to_dict() for player in all_players]
     return jsonify(all_players_serialized)
+
+
+@app.route("/players/<int:id>", methods=["GET"])
+def retrieve_player_by_id(id):
+    player = Player.query.get(id)
+    if player == None:
+        return jsonify({'error': 'no player with this id'})
+    player_serialized = player.to_dict()
+    return jsonify(player_serialized)
+
+
+@app.route("/players", methods=["POST"])
+def add_player():
+    data = request.get_json()
+    print(data)
+
+    if not data:
+        return jsonify({'error': 'Invalid or missing JSON'}), 400
+
+    required_fields = ['name']
+    for field in required_fields:
+        if field not in data or not data[field]:
+            return jsonify({'error': f'Missing required field: {field}'}), 400
+
+    name = data['name']
+    club = data.get('club',None)
+    position = data.get("position",None)
+    print(f"Club: {position}")
+    age = data.get('age',None)
+    matches_played = data.get('matches_played',None)
+    starting_11 = data.get('starting_11',None)
+    minutes = data.get('minutes',None)
+    goals = data.get('goals',None)
+    assists = data.get('assists',None)
+    goal_contributions = data.get('goal_contributions',None)
+    yellow_cards = data.get('yellow_cards',None)
+    red_cards = data.get('red_cards',None)
+    xG = data.get('xG',None)
+    non_penalty_xG = data.get('non_penalty_xG',None)
+    xA = data.get('xA',None)
+    x_goal_nonpen_contributions =data.get('x_goal_nonpen_contributions',None)
+    progressive_carries = data.get('progressive_carries',None)
+    progressive_passes = data.get('progressive_passes',None)
+    progressive_passes_received = data.get('progressive_passes_received',None)
+
+    new_player = Player(
+        club = club,
+        name = name,
+        position = position,
+        age = age,
+        matches_played = matches_played,
+        starting_11 = starting_11,
+        minutes=minutes,
+        goals=goals,
+        assists=assists,
+        goal_contributions=goal_contributions,
+        yellow_cards=yellow_cards,
+        red_cards=red_cards,
+        xG=xG,
+        non_penalty_xG=non_penalty_xG,
+        xA=xA,
+        x_goal_nonpen_contributions=x_goal_nonpen_contributions,
+        progressive_carries=progressive_carries,
+        progressive_passes=progressive_passes,
+        progressive_passes_received=progressive_passes_received
+        )
+
+    db.session.add(new_player)
+    db.session.commit()
+
+    return jsonify(new_player.serialize())
 
 
 if __name__ == "__main__":
--- player_model.py
+++ player_model.py
@@ -14,27 +14,30 @@ db = SQLAlchemy(model_class=Base)
 class Player(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
-    club: Mapped[str] = mapped_column()
-    position: Mapped[str] = mapped_column()
-    age: Mapped[str] = mapped_column()
-    matches_played: Mapped[int] = mapped_column()
-    starting_11: Mapped[int] = mapped_column()
-    minutes: Mapped[int] = mapped_column()
-    goals: Mapped[int] = mapped_column()
-    assists: Mapped[int] = mapped_column()
-    goal_contributions: Mapped[int] = mapped_column()
-    yellow_cards: Mapped[int] = mapped_column()
-    red_cards: Mapped[int] = mapped_column()
-    xG: Mapped[float] = mapped_column()
-    non_penalty_xG: Mapped[float] = mapped_column()
-    xA: Mapped[float] = mapped_column()
-    x_goal_nonpen_contributions: Mapped[float] = mapped_column()
-    progressive_carries: Mapped[float] = mapped_column()
-    progressive_passes: Mapped[float] = mapped_column()
-    progressive_passes_received: Mapped[float] = mapped_column()
+    club: Mapped[str] = mapped_column(nullable=True)
+    position: Mapped[str] = mapped_column(nullable=True)
+    age: Mapped[str] = mapped_column(nullable=True)
+    matches_played: Mapped[int] = mapped_column(nullable=True)
+    starting_11: Mapped[int] = mapped_column(nullable=True)
+    minutes: Mapped[int] = mapped_column(nullable=True)
+    goals: Mapped[int] = mapped_column(nullable=True)
+    assists: Mapped[int] = mapped_column(nullable=True)
+    goal_contributions: Mapped[int] = mapped_column(nullable=True)
+    yellow_cards: Mapped[int] = mapped_column(nullable=True)
+    red_cards: Mapped[int] = mapped_column(nullable=True)
+    xG: Mapped[float] = mapped_column(nullable=True)
+    non_penalty_xG: Mapped[float] = mapped_column(nullable=True)
+    xA: Mapped[float] = mapped_column(nullable=True)
+    x_goal_nonpen_contributions: Mapped[float] = mapped_column(nullable=True)
+    progressive_carries: Mapped[float] = mapped_column(nullable=True)
+    progressive_passes: Mapped[float] = mapped_column(nullable=True)
+    progressive_passes_received: Mapped[float] = mapped_column(nullable=True)
 
     def to_dict(self):
         dictionary = {}
         for column in self.__table__.columns:
             dictionary[column.name] = getattr(self,column.name)
         return dictionary
+
+    def serialize(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
